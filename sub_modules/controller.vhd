2bd281ce5f068725efacf754f2460ca239800dc8
Brett Dionello <bdionello@uvic.ca>
2025-02-28 22:58:07 -0800

    initial top level design

--- sub_modules/controller.vhd
+++ sub_modules/controller.vhd
@@ -4,9 +4,26 @@ use ieee.std_logic_1164.all ;
 use work.cpu_types.all;
 
 entity controller is
-    port (clk, reset: in STD_LOGIC;
-    y: out STD_LOGIC);
-end controller ;
+    port (
+    -- inputs    
+    clk : in STD_LOGIC;
+    reset_ex: in STD_LOGIC;
+    reset_ld: in STD_LOGIC;
+    inst_in : IN std_logic_vector (15 downto 0);
+    -- outputs
+    reg_dst : OUT STD_LOGIC;
+    branch : OUT STD_LOGIC;
+    RAM_mem_rd : OUT STD_LOGIC;
+    RAM_mem_to_reg : OUT STD_LOGIC;
+    ROM_mem_rd : OUT STD_LOGIC;
+    ROM_mem_to_reg : OUT STD_LOGIC;
+    alu_op : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+    RAM_mem_wr : OUT STD_LOGIC;
+    ROM_mem_wr : OUT STD_LOGIC;
+    alu_src : OUT STD_LOGIC;
+    reg_wr : OUT STD_LOGIC
+    );
+    end controller ;
 
 architecture rtl of controller is    
     signal state, nextstate: statetype;
@@ -17,15 +34,15 @@ begin
     S1 when state = S0 else
     S2 when state = S1 else
     S0;
-
     -- state register
-    process (clk, reset) begin
-        if reset = '1' then state <= S0;    
+    process (clk, reset_ex, reset_ld) begin
+        -- Reset load means ROM mode
+        -- Reset Execute means RAM mode
+        if reset_ld = '1' then state <= S0;    
         elsif clk'event and clk = '1' then    
             state <= nextstate;    
         end if;    
     end process;
-
      -- controller outputs
-     y <= '1 when state = S0 else '0'';
+
 end rtl ;
