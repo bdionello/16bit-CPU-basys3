78897fb44371ab3d4677c1df99d35f69e460a4a8
Brett Dionello <bdionello@uvic.ca>
2025-03-03 11:36:29 -0800

    added temp_result_mul for 16bit inputs to 32bit output

--- sub_modules/alu.vhd
+++ sub_modules/alu.vhd
@@ -23,13 +23,14 @@ architecture behavioral of ALU is
 begin
     process(in1, in2, shift, alu_mode)
     variable temp_result : std_logic_vector(15 downto 0);
+    variable temp_result_mul : std_logic_vector(31 downto 0);
     begin
         case alu_mode is
             -- A-Format Instructions
             when "0000000" => temp_result := (others => '0');                                                           -- NOP (A0 Format)
             when "0000001" => temp_result := std_logic_vector(signed(in1) + signed(in2));                               -- ADD (A1 Format)
             when "0000010" => temp_result := std_logic_vector(signed(in1) - signed(in2));                               -- SUB (A1 Format)
-            when "0000011" => temp_result := std_logic_vector((signed(in1 (7 downto 0)) * signed(in2 (7 downto 0))));                               -- MUL (A1 Format)
+            when "0000011" => temp_result_mul := std_logic_vector(signed(in1) * signed(in2));                               -- MUL (A1 Format)
             when "0000100" => temp_result := in1 NAND in2;                                                              -- NAND (A1 Format)
             when "0000101" => temp_result := std_logic_vector(shift_left(unsigned(in1), to_integer(unsigned(shift))));  -- SHL (A2 Format)
             when "0000110" => temp_result := std_logic_vector(shift_right(unsigned(in1), to_integer(unsigned(shift)))); -- SHR (A2 Format)
@@ -54,7 +55,11 @@ begin
         end case;
 
         -- Output result
-        alu_out <= temp_result;
+        if (alu_mode = "0000011") then
+            alu_out <= temp_result_mul (15 downto 0); -- does not check for overflow        
+        else        
+            alu_out <= temp_result;
+        end if;
        
         -- Set Flags
         if (temp_result = "0000000000000000") then
