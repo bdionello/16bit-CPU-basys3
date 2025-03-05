857a033060822ddd7e5e7d4a085ee8dcf2e5fa50
Brett Dionello <bdionello@uvic.ca>
2025-03-05 00:27:16 -0800

    removed bootload mode

--- sub_modules/mem_manager.vhd
+++ sub_modules/mem_manager.vhd
@@ -13,7 +13,6 @@ use work.cpu_types.all;
 entity mem_manager is
     Port (
         -- Shared ports
-        bootload_mode : in STD_LOGIC; -- asserted means reset load mode 
         data_in : in STD_LOGIC_VECTOR (15 downto 0); -- All write data
         write_enable : in STD_LOGIC;
         read_data_enable : in STD_LOGIC;
@@ -38,7 +37,6 @@ architecture mem_manager_arch of mem_manager is
     signal rom_enable_i : STD_LOGIC;    
     signal ram_a_enable_i : STD_LOGIC;        
     signal ram_b_enable_i : STD_LOGIC;       
-    ----
     -- Data Signals
     signal out_port_i : STD_LOGIC_VECTOR (15 downto 0); 
     signal data_addr_i : STD_LOGIC_VECTOR (15 downto 0);
@@ -47,9 +45,7 @@ architecture mem_manager_arch of mem_manager is
     signal ram_a_data_out_i : STD_LOGIC_VECTOR (15 downto 0);
     signal ram_b_data_out_i : STD_LOGIC_VECTOR (15 downto 0);     
            
-begin
-           
-    -------------------
+begin           
     -- Internal State Logic
     reset_i <= '1' when (reset = '1') else '0';    
     rom_enable_i <= '1' when (read_inst_enable = '1') AND (data_addr AND X"0400") = X"0000" else 
@@ -58,15 +54,14 @@ begin
     ram_a_enable_i <= '1' when (read_data_enable = '1') AND (data_addr AND X"0400") = X"0400" else
                       '0' when (reset = '1') else
                       '0';
-    ram_b_enable_i <= '1' when (read_inst_enable = '1') AND (bootload_mode = '1') else
+    ram_b_enable_i <= '1' when (read_inst_enable = '1') AND (data_addr AND X"0400") = X"0400" else
                       '0' when (reset = '1') else
                       '0';
     write_enable_i <= "1" when (write_enable = '1') else
                       "0" when (reset = '1') else
                       "0";     
-    ------------------- 
     -- OUTPUT State LOGIC                 
-    inst_out <= ram_b_data_out_i when (data_addr AND X"0400") = X"0400" AND (bootload_mode = '0') else rom_data_out_i;                
+    inst_out <= ram_b_data_out_i when (data_addr AND X"0400") = X"0400"  else rom_data_out_i;                
     data_out <= in_port when (data_addr = X"FFF2") AND (read_data_enable = '1') else ram_a_data_out_i;                                    
     out_port <= data_in when (data_addr = X"FFF0") AND (write_enable = '1') else X"0000";
     
